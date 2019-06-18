9727bdc0bf86530875b0bf969171ab3404e5168f
neu <neu1neu9neu9neu9neu@gmail.com>
2019-06-18 11:01:33 +0900

    format file: src/*

--- src/pipeline/mod.rs
+++ src/pipeline/mod.rs
@@ -6,11 +6,13 @@ pub struct Data<T> {
 
 impl<T> Data<T> {
     pub fn new(data: T) -> Data<T> {
-        Data{data}
+        Data { data }
     }
 
     pub fn adapt<P, O>(self, process: P) -> Data<O>
-        where P: Fn(T) -> O {
+    where
+        P: Fn(T) -> O,
+    {
         unimplemented!();
     }
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -7,8 +7,5 @@ fn it_works() {
 
 #[test]
 fn data_new_u32vec() {
-    assert_eq!(
-        pipeline::Data::new(vec![1,2,3]).data,
-        vec![1,2,3]
-    );
+    assert_eq!(pipeline::Data::new(vec![1, 2, 3]).data, vec![1, 2, 3]);
 }
